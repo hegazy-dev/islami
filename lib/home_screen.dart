4352c86c4690b71b0e3df56f9f0074e25d3445b9
hegazy-dev <mahmoud.hegazy.dev@gmail.com>
2026-07-07 01:58:22 +0300

    add bottom nav bar

---
+++ lib/home_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:islami/active_nav_bar_icon.dart';
+import 'package:islami/inactive_nav_bar_icon.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          currentIndex = index;
+          setState(() {});
+        },
+        backgroundColor: Color(0xFFE2BE7F),
+        type: .fixed,
+        showUnselectedLabels: false,
+        selectedItemColor: Color(0xFF202020),
+        items: [
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: 'quran'),
+            activeIcon: ActiveNavBarIcon(iconName: 'quran'),
+            label: 'Quran',
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: 'hadith'),
+            activeIcon: ActiveNavBarIcon(iconName: 'hadith'),
+            label: 'Hadith',
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: 'sebha'),
+            activeIcon: ActiveNavBarIcon(iconName: 'sebha'),
+            label: 'Sebha',
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: 'radio'),
+            activeIcon: ActiveNavBarIcon(iconName: 'radio'),
+            label: 'Radio',
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: 'time'),
+            activeIcon: ActiveNavBarIcon(iconName: 'time'),
+            label: 'Time',
+          ),
+        ],
+      ),
+    );
+  }
+}
